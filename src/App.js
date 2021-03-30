b5e1d275c33292639332e102480f8c5ba1fb8d5a
Sumit Saini <sumitsaini199711@gmail.com>
2021-03-30 18:57:18 +0530

    Added navbar basic style

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Contact from "./components/Contact/Contact";
 import Education from "./components/Education/Education";
 import Experience from "./components/Experience/Experience";
 import Home from "./components/Home/Home";
-import Nav from "./components/Nav/Nav";
+import NavHeader from "./components/Nav/NavHeader";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
-      <Nav />
+      <NavHeader />
       <Home />
       <AboutMe />
       <Experience />
---
+++ src/components/Nav/NavHeader.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "./NavHeader.css";
+export class NavHeader extends Component {
+  render() {
+    return (
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="ml-auto mr-5">
+          <Nav.Link className="nav-bar-link" href="#home">
+            Home
+          </Nav.Link>
+          <Nav.Link className="nav-bar-link" href="#features">
+            About
+          </Nav.Link>
+          <Nav.Link className="nav-bar-link" href="#pricing">
+            Experience
+          </Nav.Link>
+          <Nav.Link className="nav-bar-link" href="#pricing">
+            Project
+          </Nav.Link>
+          <Nav.Link className="nav-bar-link" href="#pricing">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+    );
+  }
+}
+
+export default NavHeader;
