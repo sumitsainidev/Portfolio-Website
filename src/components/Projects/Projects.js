f243301a1c58ebdc8c56d84d664e234107d824e0
Sumit Saini <sumitsaini199711@gmail.com>
2021-04-02 23:15:39 +0530

    Modifying Projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,40 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import Section from "../components/Section";
+import Card from "../components/Card";
+import { useState } from "react";
+import { connect } from "react-redux";
 
-export class Projects extends Component {
-  render() {
-    return <div>Projects Component</div>;
-  }
-}
+const Projects = ({ portfolios }) => {
+  const [count, setCount] = useState(8);
+  const [items, setItems] = useState([]);
 
-export default Projects;
+  useEffect(() => {
+    setItems(portfolios.slice(0, count));
+  }, [count, portfolios]);
+
+  return (
+    <Section id="portfolio" title="Major Open Source Projects">
+      <div className="cards">
+        {items && items.map((card, index) => <Card card={card} key={index} />)}
+      </div>
+
+      <div className="d-flex justify-content-center">
+        {count < portfolios.length && (
+          <div
+            className="btn btn-primary mt-5 btn-lg"
+            onClick={() => setCount(count + 4)}
+          >
+            See more
+          </div>
+        )}
+      </div>
+    </Section>
+  );
+};
+
+const mapStateToProps = (store) => {
+  const { portfolios } = store.portfolio;
+  return { portfolios };
+};
+
+export default connect(mapStateToProps)(Projects);
