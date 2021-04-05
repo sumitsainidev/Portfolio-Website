f21d8d8df86cd7343115882b3dfc4935694966f6
Sumit Saini <sumitsaini199711@gmail.com>
2021-04-05 13:02:47 +0530

    Improving Contact component Styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -13,93 +13,98 @@ const Contact = () => {
             </h1>
           </div>
         </>
-        <div className="">
-          <div className="git-cont row">
-            <div className=".col-md-offset-4 col-6 col-sm-6 half">
-              <form
-                action={
-                  contact.contactUrl
-                    ? contact.contactUrl
-                    : "https://formspree.io"
-                }
-                method={contact.contactUrl ? "POST" : "GET"}
+        <div className="flex-container">
+          <div className="half flex-item-left">
+            <form
+              action={
+                contact.contactUrl ? contact.contactUrl : "https://formspree.io"
+              }
+              method={contact.contactUrl ? "POST" : "GET"}
+            >
+              <input
+                type="text"
+                id="fname"
+                name="firstname"
+                placeholder="Your name"
+                required
+              ></input>
+              <input
+                type="mail"
+                id="mailid"
+                name="Email"
+                placeholder="Email Address"
+                required
+              ></input>
+              <input
+                type="text"
+                id="sub"
+                name="Subject"
+                placeholder="Subject"
+                required
+              ></input>
+              <textarea
+                id="msg"
+                className="mar10"
+                name="message"
+                placeholder="Message"
+                required
+              ></textarea>
+              <Button
+                className="btn btn-lg buttonBack"
+                style={{ marginLeft: "10px" }}
               >
-                <input
-                  type="text"
-                  id="fname"
-                  name="firstname"
-                  placeholder="Your name"
-                  required
-                ></input>
-                <input
-                  type="mail"
-                  id="mailid"
-                  name="Email"
-                  placeholder="Email Address"
-                  required
-                ></input>
-                <input
-                  type="text"
-                  id="sub"
-                  name="Subject"
-                  placeholder="Subject"
-                  required
-                ></input>
-                <textarea
-                  id="msg"
-                  className="mar10"
-                  name="message"
-                  placeholder="Message"
-                  required
-                ></textarea>
-                <Button
-                  className="btn btn-lg buttonBack"
-                  style={{ marginLeft: "10px" }}
-                >
-                  Submit
-                </Button>
-              </form>
-            </div>
-            <div className=".col-md-offset-4 col-6 col-sm-6 half">
-              <p className="lead">{contact.pitch}</p>
-              <div className="d-flex justify-content-center align-items-center flex-column">
-                <div className="inline-block" style={{ paddingTop: "48px" }}>
-                  {socialMediaLinks.linkedin && (
-                    <a
-                      title="Visit Linkedin profile"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      href={socialMediaLinks.linkedin}
-                    >
-                      <i className="fab fa-linkedin"></i>
-                    </a>
-                  )}
-                  {socialMediaLinks.github && (
-                    <a
-                      title="Visit Github profile"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      href={socialMediaLinks.github}
-                    >
-                      <i className="fab fa-github"></i>
-                    </a>
-                  )}
-                  <br />
-                </div>
-                {socialMediaLinks.resume && (
-                  <a title="Download Resume" href="www.google.com" download>
-                    <i className="fas fa-download"></i>
+                Submit
+              </Button>
+            </form>
+          </div>
+          <div className="half flex-item-right">
+            <p className="lead">{contact.pitch}</p>
+            <div className="d-flex justify-content-center align-items-center flex-column">
+              <div
+                className="inline-block"
+                style={{ paddingTop: "5px", fontSize: "42px", display: "flex" }}
+              >
+                {socialMediaLinks.linkedin && (
+                  <a
+                    title="Visit Linkedin profile"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href={socialMediaLinks.linkedin}
+                    style={{ width: "50px" }}
+                  >
+                    <i className="fab fa-linkedin"></i>
                   </a>
                 )}
+                {socialMediaLinks.github && (
+                  <a
+                    title="Visit Github profile"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href={socialMediaLinks.github}
+                    style={{ width: "50px" }}
+                  >
+                    <i className="fab fa-github"></i>
+                  </a>
+                )}
+                <br />
               </div>
+
+              <a
+                title="Download Resume"
+                href="www.google.com"
+                style={{ paddingTop: "5px", fontSize: "42px", width: "50px" }}
+                download
+              >
+                <i className="fas fa-download"></i>
+              </a>
             </div>
           </div>
         </div>
+        <p id="not-dark" className="Copy">
+          2020 © Copyright <strong>{contact.copyright}</strong>. All Rights
+          Reserved
+        </p>
       </div>
-      <p id="not-dark" className="Copy">
-        2020 © Copyright <strong>{contact.copyright}</strong>. All Rights
-        Reserved
-      </p>
     </div>
   );
 };
